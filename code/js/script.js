53fe8082357a6da1440a0728052c6a7d114b9cea
hamattiv2 <makotarov2@gmail.com>
2019-05-27 22:52:41 +0900

    no message

--- code/js/script.js
+++ code/js/script.js
@@ -1,10 +1,8 @@
-$(function(){
   $('.slider').slick({
   autoplay: true,
     autoplaySpeed: 5000,
     dots:true,
     fade: true,
-    prevArrow: '<button class="slide-arrow prev-arrow">prev</button>',
-    nextArrow: '<button class="slide-arrow next-arrow">next</button>'
+    prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+    nextArrow: '<button class="slide-arrow next-arrow"></button>'
   });
-});
