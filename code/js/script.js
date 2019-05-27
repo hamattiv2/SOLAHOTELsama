f752d8ed9e6450dc6a278848d983a1d93b4a0f75
hamattiv2 <50069366+hamattiv2@users.noreply.github.com>
2019-05-27 23:11:49 +0900

    Revert "こみっと"

--- code/js/script.js
+++
@@ -1,8 +0,0 @@
-  $('.slider').slick({
-  autoplay: true,
-    autoplaySpeed: 5000,
-    dots:true,
-    fade: true,
-    prevArrow: '<button class="slide-arrow prev-arrow"></button>',
-    nextArrow: '<button class="slide-arrow next-arrow"></button>'
-  });
